9bdaee30f48317b0ad7f24e9fd8761919d161792
Easton Potokar <contagon6@gmail.com>
2020-09-02 15:20:31 -0600

    Small tweaks

--- datagen.py
+++ datagen.py
@@ -1,49 +1,44 @@
 import holodeck
 from holodeck.environments import *
 import numpy as np
-import keyboard
 import sys
-# np.set_printoptions(precision=3)
-config = {
-    "name": "DataGen",
-    "world": "Rooms",
-    "main_agent": "turtle0",
-    "agents":[
-        {
-            "agent_name": "turtle0",
-            "agent_type": "TurtleAgent",
-            "sensors": [
-				{
-                    "sensor_type": "LocationSensor"
-                },
-                {
-                    "sensor_type": "RotationSensor"
-                },
-                {
-                    "sensor_type": "RangeFinderSensor",
-                    "configuration": {
-                        "LaserCount": 64,
-                        "LaserMaxDistance": 10,
-                        "LaserAngle": 0,
-						"LaserDebug": True
-                    }
-                }
-            ],
-            "control_scheme": 0,
-            "location": [1.50, 1.50, 1.0],
-            "rotation": [0.0, 0.0, 0.0]
-        }
-    ],
+from pynput import keyboard
 
-    "window_width":  1280,
-    "window_height": 720
-}
 def a2s(x):
     return ' '.join(str(i) for i in x)
 
+command = np.array([0, 0])
+
+def on_press(key):
+    try:
+        if key.char == "a":
+            command[1] = -1
+        if key.char == "d":
+            command[1] = 1
+        if key.char == "w":
+            command[0] = 70
+        if key.char == "s":
+            command[0] = -70
+    except AttributeError:
+        print('special key {0} pressed'.format(
+            key))
+
+def on_release(key):
+    if key.char == "a":
+        command[1] = 0
+    if key.char == "d":
+        command[1] = 0
+    if key.char == "w":
+        command[0] = 0
+    if key.char == "s":
+        command[0] = 0
+    if key.char == "q":
+        command[0] = 10000
+
+
 #set up holodeck
 env = holodeck.make("Rooms-DataGen")
-# env = HolodeckEnvironment(scenario=config, start_world=False)
+# env = HolodeckEnvironment(scenario=config, start_world=True)
 env.reset()
 
 #get file ready
@@ -56,22 +51,19 @@ num_angles = 64
 angles = np.linspace(0, 360, num_angles)
 output.write(f"{num_angles} {a2s(angles)}\n")
 
+# ...or, in a non-blocking fashion:
+listener = keyboard.Listener(
+    on_press=on_press,
+    on_release=on_release)
+listener.start()
+
 i=-1
 while True:
     i+=1
-    #get input
-    command = np.array([0, 0])
-    if keyboard.is_pressed('w'):
-        command[0] += 70
-    if keyboard.is_pressed('s'):
-        command[0] -= 70
-    if keyboard.is_pressed('a'):
-        command[1] -= 1
-    if keyboard.is_pressed('d'):
-        command[1] += 1
 
-    if keyboard.is_pressed('q'):
+    if command[0] == 10000:
         break
+    print(command)
 
     #send to holodeck
     env.act("turtle0", command)
